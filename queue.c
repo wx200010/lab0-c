7ec6f4ee3ed5698a801e703cf1df4b83a407bea1
Shao-Huan Yu <wx200010@gmail.com>
2025-03-02 21:36:55 +0800

    Fix q_size to return the correct value
    The q_size function always returned -1, which is not the number of
    elements in the queue.
    
    To fix this, I used the macro "list_for_each" to iterate through the
    queue starting from head, counted the number of elements traversed, and
    returned that value.
    
    Change-Id: I1d0fc6c14cefcee1f2f77dec62306d6eb38c401b

--- queue.c
+++ queue.c
@@ -40,7 +40,16 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return 0;
+
+    int len = 0;
+    struct list_head *it;
+
+    list_for_each (it, head)
+        len++;
+
+    return len;
 }
 
 /* Delete the middle node in queue */
