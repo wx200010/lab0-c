95096edd077febeb7070b5b820f227f9b8e6d633
Shao-Huan Yu <wx200010@gmail.com>
2025-03-03 16:27:41 +0800

    Implement q_new and q_free functions
    Previously, these functions were not implemented.
    
    q_new allocates memory using "malloc" and initializes the list head with
    "INIT_LIST_HEAD".
    
    q_free iterates through the queue, freeing each node. Since the nodes
    will be freed, updating the next and previous pointers is unnecessary.
    
    Change-Id: I53d990f70c75ef0e5c093aee36bc94101c11e16b

--- queue.c
+++ queue.c
@@ -7,11 +7,27 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head)
+        return NULL;
+
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    element_t *curr = NULL, *next = NULL;
+    list_for_each_entry_safe (curr, next, head, list) {
+        free(curr->value);
+        free(curr);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
